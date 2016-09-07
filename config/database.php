fb50fd845e3e2c54ded598e890d8e5dce64debff
Alain DEMENET <ademenet@student.42.fr>
2016-09-07 17:51:29 +0200

    fichier tel que demande par le sujet

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<!--
+@Author: Alain Demenet <ademenet>
+@Date:   Sep 07 2016
+@Email:  ademenet@student.42.fr
+@Project: Camagru
+@Last modified by:   ademenet
+@Last modified time: Sep 07 2016
+-->
+
+<?php
+
+$DB_DSN = 'mysql:dbname=camagru;host=localhost;port=8080';
+$DB_USR = 'root';
+$DB_PASSWORD = 'root';
